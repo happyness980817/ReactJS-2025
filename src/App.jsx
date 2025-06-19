dbf63eaa3895b0d947896381db56bc9368e074f9
Paul Jang <happyness980817@gmail.com>
2025-06-19 11:25:45 +0900

    state functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function App() {
   const [counter, setCouter] = React.useState(0);
   const onClick = () => {
-    setCouter(counter + 1);
+    // setCouter(counter + 1);
+    // useState 의 setter function 은 함수 또는 state 를 parameter 로 받을 수 있음.
+    // 함수일 경우 현재 state 를 parameter 로 전달
+    setCouter((current) => current + 1);
   };
   return (
     <div>
