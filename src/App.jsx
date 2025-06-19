6af1222294c559b8bfab8ada13bb484426a2433f
Paul Jang <happyness980817@gmail.com>
2025-06-19 12:50:13 +0900

    units converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,83 @@
 import React from "react";
 
-function App() {
-  const [minutes, setMinutes] = React.useState();
-  const onChange = (event) => {
-    setMinutes(event.target.value);
+// 별도의 컴포넌트로 분리
+
+function MinutesConverter() {
+  const [minutes, setMinutes] = React.useState("");
+
+  const onChange = (e) => {
+    setMinutes(e.target.value);
   };
+
+  const reset = () => {
+    setMinutes(0);
+  };
+
   return (
     <div>
-      <h1>Units Converter</h1>
-      <label htmlFor="minutes">Minutes</label>
       <input
         value={minutes}
         id="minutes"
         placeholder="Minutes"
         type="number"
-        onChange={onChange} // event handler 들은 event listener 에 의해 호출될 때 자동으로 이벤트 객체를 파라미터로 받음
+        onChange={onChange}
       />
-      <h4>You want to convert {minutes}</h4>
+      <label htmlFor="minutes">Minutes</label>
+      <span> is </span>
+      <input
+        value={Math.round(minutes / 60)}
+        id="hours"
+        placeholder="Hours"
+        type="number"
+        disabled
+      />
       <label htmlFor="hours">Hours</label>
-      <input id="hours" placeholder="Hours" type="number" />
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+function HoursConverter() {
+  const [hours, setHours] = React.useState("");
+
+  const onChange = (e) => {
+    setHours(e.target.value);
+  };
+
+  const reset = () => {
+    setHours(0);
+  };
+
+  return (
+    <div>
+      <input
+        value={hours}
+        id="hours"
+        placeholder="Hours"
+        type="number"
+        onChange={onChange}
+      />
+      <label htmlFor="hours">Hours</label>
+      <span> is </span>
+      <input
+        value={hours * 60}
+        id="minutes"
+        placeholder="Minutes"
+        type="number"
+        disabled
+      />
+      <label htmlFor="minutes">Minutes</label>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Units Converter</h1>
+      <MinutesConverter />
+      <HoursConverter />
     </div>
   );
 }
