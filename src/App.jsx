8de5f6ca91c06a0b3604f32a5675e30853f38dc8
Paul Jang <happyness980817@gmail.com>
2025-06-19 14:10:21 +0900

    memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-// Props
-// 컴포넌트는 JSX 를 리턴해주는 함수들
-// React 컴포넌트는 props 라는 단일 객체를 유일한 매개변수로 받음
-// JSX에서 컴포넌트에 전달하는 속성들이 빈 props 객체에 key-value 쌍으로 들어감
-// (밑에 App 에서 Btn 태그 안에 text, big 속성들을 할당해서 빈 객체인 props 에 해당 속성들이 할당)
-// props는 수정할 수 없는 읽기 전용 객체
-// props 로 인해 부모 컴포넌트에서 자식 컴포넌트로 데이터를 전달할 수 있다.
+// Memo
 
-function Btn({ text, big }) {
+// 기본 동작 :
+// - 부모 컴포넌트가 리렌더링될 때 자식 컴포넌트도 항상 리렌더링
+// - props가 변경되지 않았어도 리렌더링 발생
+
+// React.memo 사용 시:
+// - props의 변경 여부를 체크
+// - props가 변경되지 않았다면 이전 렌더링 결과를 재사용
+// - 불필요한 리렌더링 방지
+
+function Btn({ text, changeValue }) {
+  console.log(text, "was rendered.");
   return (
     <button
       style={{
@@ -17,21 +21,31 @@ function Btn({ text, big }) {
         padding: "20px 30px",
         border: 0,
         borderRadius: 10,
-        fontSize: big ? 36 : 24,
       }}
+      onClick={changeValue} // 이 onClick 이 button 의 eventlistener
     >
       {text}
     </button>
   );
 }
 
+const MemorizedBtn = React.memo(Btn);
+
 function App() {
+  const [value, setValue] = React.useState("Save Changes");
+  const changeValue = () => {
+    if (value === "Save Changes") {
+      setValue("Revert Changes");
+    } else {
+      setValue("Save Changes");
+    }
+  };
   return (
     <div>
-      <Btn text="Save Changes" big={true} />
-      <Btn text="Continue" />
+      <MemorizedBtn text={value} changeValue={changeValue} />
+      <MemorizedBtn text="Continue" />
     </div>
-  );
+  ); // 여기 있는 changeValue 는 eventlistener 가 아니라 props 임!!!
 }
 
 export default App;
