6cc3f022afa2b655d17749e6a83dceadebee8c52
Paul Jang <happyness980817@gmail.com>
2025-06-19 11:31:03 +0900

    state functions

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     // setCouter(counter + 1);
     // useState 의 setter function 은 함수 또는 state 를 parameter 로 받을 수 있음.
     // 함수일 경우 현재 state 를 parameter 로 전달
+    // setter 내부에 함수가 있으면 함수 파라미터가 현재 state 값으로 인식되고, 함수가 리턴하는 값이 새로운 state 값으로 바뀐다
     setCouter((current) => current + 1);
   };
   return (
