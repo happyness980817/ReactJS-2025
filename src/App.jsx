2cd791d7f278e47ec57a49a62f5c05a965df0487
Paul Jang <happyness980817@gmail.com>
2025-06-19 21:27:03 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// useEffect
+
+// useEffect is a function that takes two arguments.
+// 첫번째 argument 로 한번만 렌더링하고 싶은 함수를 받는다
+// 원래는 state 바뀔때마다 전체 코드가 렌더링됨
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("api called."); // don't want to run this code every time whenever a state changes.
+  console.log("I run all the time.");
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
