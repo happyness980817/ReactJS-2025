437f9b64a93aab04acbad6c64a1963e275e4fc3c
Paul Jang <happyness980817@gmail.com>
2025-06-19 13:30:13 +0900

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,35 @@
 import React from "react";
 
-// 별도의 컴포넌트로 분리
+// Props
+// 컴포넌트는 JSX 를 리턴해주는 함수들
+// React 컴포넌트는 props 라는 단일 객체를 유일한 매개변수로 받음
+// JSX에서 컴포넌트에 전달하는 속성들이 빈 props 객체에 key-value 쌍으로 들어감
+// (밑에 App 에서 Btn 태그 안에 text, big 속성들을 할당해서 빈 객체인 props 에 해당 속성들이 할당)
+// props는 수정할 수 없는 읽기 전용 객체
+// props 로 인해 부모 컴포넌트에서 자식 컴포넌트로 데이터를 전달할 수 있다.
 
-function MinutesConverter() {
-  const [minutes, setMinutes] = React.useState("");
-
-  const onChange = (e) => {
-    setMinutes(e.target.value);
-  };
-
-  const reset = () => {
-    setMinutes(0);
-  };
-
+function Btn({ text, big }) {
   return (
-    <div>
-      <input
-        value={minutes}
-        id="minutes"
-        placeholder="Minutes"
-        type="number"
-        onChange={onChange}
-      />
-      <label htmlFor="minutes">Minutes</label>
-      <span> is </span>
-      <input
-        value={Math.round(minutes / 60)}
-        id="hours"
-        placeholder="Hours"
-        type="number"
-        disabled
-      />
-      <label htmlFor="hours">Hours</label>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-}
-
-function HoursConverter() {
-  const [hours, setHours] = React.useState("");
-
-  const onChange = (e) => {
-    setHours(e.target.value);
-  };
-
-  const reset = () => {
-    setHours(0);
-  };
-
-  return (
-    <div>
-      <input
-        value={hours}
-        id="hours"
-        placeholder="Hours"
-        type="number"
-        onChange={onChange}
-      />
-      <label htmlFor="hours">Hours</label>
-      <span> is </span>
-      <input
-        value={hours * 60}
-        id="minutes"
-        placeholder="Minutes"
-        type="number"
-        disabled
-      />
-      <label htmlFor="minutes">Minutes</label>
-      <button onClick={reset}>Reset</button>
-    </div>
+    <button
+      style={{
+        backgroundColor: "tomato",
+        color: "white",
+        padding: "20px 30px",
+        border: 0,
+        borderRadius: 10,
+        fontSize: big ? 36 : 24,
+      }}
+    >
+      {text}
+    </button>
   );
 }
 
 function App() {
   return (
     <div>
-      <h1>Units Converter</h1>
-      <MinutesConverter />
-      <HoursConverter />
+      <Btn text="Save Changes" big={true} />
+      <Btn text="Continue" />
     </div>
   );
 }
