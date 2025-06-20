ca7581f6e70175c5010f28ef923e8cbad554998b
Paul Jang <happyness980817@gmail.com>
2025-06-20 19:47:09 +0900

    to do list (1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState, useEffect } from "react";
 
-// useEffect
-
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time.");
-  useEffect(() => {
-    console.log("CALLED THE API!!!");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 1) {
-      console.log("SEARCH FOR", keyword);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]); // keyword 가 변화할 때마다 첫번째 argument 코드 실행
-  useEffect(() => {
-    console.log("I run when the 'counter' OR 'keyword' changes. ");
-  }, [counter, keyword]);
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click</button>
+      <h1>My To Dos</h1>
+      <h4>You have {toDos.length} thins to do!!!</h4>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
