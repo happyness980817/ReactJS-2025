953fc02d8efe20235f2c885d3a1bbbd8328517a6
Paul Jang <happyness980817@gmail.com>
2025-06-20 17:51:56 +0900

    useeffect (2) - second argument

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { useState, useEffect } from "react";
 
 // useEffect
 
-// useEffect is a function that takes two arguments.
-// 첫번째 argument 로 한번만 렌더링하고 싶은 함수를 받는다
-// 원래는 state 바뀔때마다 전체 코드가 렌더링됨
-
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time.");
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("CALLED THE API!!!");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 1) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]); // keyword 가 변화할 때마다 첫번째 argument 코드 실행
+  useEffect(() => {
+    console.log("I run when the 'counter' OR 'keyword' changes. ");
+  }, [counter, keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click</button>
     </div>
